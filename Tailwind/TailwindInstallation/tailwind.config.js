6a421a1ddebe5805a8b33f4f036f3fd4c7f627df
suraj59357 <surajkumar59357@gmail.com>
2026-01-08 22:16:57 +0530

    TailwindDay1 & RazorpayNavbar

---
+++ Tailwind/TailwindInstallation/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+  // "./index.html",
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+
+
+
+
